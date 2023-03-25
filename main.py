33a7b5423cf6785f0d5cdc1c076225a3eacbdbe0
Elena-Gomez-Orihuel <elenamgomezorihuel@gmail.com>
2023-03-25 09:47:24 +0100

    Succesful start of web scrapping: Cookies button clicked

--- main.py
+++ main.py
@@ -1,84 +1,52 @@
-# This is a sample Python script.
-
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-#pip install pandas
-#pip install requests
-#pip install beautifulsoup4
-
-import requests
-from bs4 import BeautifulSoup
+import csv
+import time
 import pandas as pd
+import re
+from calendar import monthrange
+import datetime
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
+import warnings
+
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    warnings.filterwarnings("ignore", category=DeprecationWarning)
+    #Navigation options
+    options = Options()
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument("--start-maximized")
+
+    #Opening investing.com
+    #Navigator initialization
+    #driver.get('https://www.investing.com/etfs/ishares-global-corporate-bond-$')
+    with webdriver.Chrome(service=Service(executable_path=ChromeDriverManager().install()), options=options) as driver:
+        #for asset in assets:
+        driver.get("https://www.investing.com/etfs/ishares-global-corporate-bond-$")
+
+        #Cookies button
+        #// *[ @ id = "onetrust-accept-btn-handler"]
+        cookies_button = WebDriverWait(driver,60).until(EC.element_to_be_clickable((By.ID,"onetrust-accept-btn-handler")))
+        cookies_button.click()
+        time.sleep(1000)
 
 
-    # Define the URL for the assets
-    url_stocks = 'https://www.investing.com/equities/amundi-msci-wrld-ae-c-historical-data'
-    url_corporate_bonds = 'https://www.investing.com/etfs/ishares-global-corporate-bond-usd-historical-data'
-    url_public_bonds = 'https://www.investing.com/etfs/db-x-trackers-ii-global-sovereign-5-historical-data'
-    url_gold = 'https://www.investing.com/etfs/spdr-gold-trust-historical-data'
-    url_cash = 'https://www.investing.com/currencies/us-dollar-index-historical-data'
+    #webdriver_options = Options()
 
-    # Define the date range for the data
-    start_date = '01/01/2020'
-    end_date = '12/31/2020'
 
-    # Define the payload parameters
-    params = {
-        'historical_data_start_date': start_date,
-        'historical_data_end_date': end_date,
-        'interval_sec': 'Daily',
-        'action': 'historical_data'
-    }
+    ##### FIRST PART #####
 
-    # Make the request to the website and extract the data
-    #STOCK
-    response_stock = requests.post(url_stocks, data=params)
-    soup_stocks = BeautifulSoup(response_stock.content, 'html.parser')
-    table_stocks = soup_stocks.find_all('table')[0]
-    df = pd.read_html(str(table_stocks))[0]
-    # Save the data to a CSV file
-    filename_stocks = 'amundi-msci-wrld-ae-c.csv'
-    df.to_csv(filename_stocks, index=False)
-    #CORPORATE BONDS
-    response_corporate_bonds = requests.post(url_corporate_bonds, data=params)
-    soup_corporate_bonds = BeautifulSoup(response_corporate_bonds.content, 'html.parser')
-    table_corporate_bonds = soup_corporate_bonds.find_all('table')[0]
-    df_corporate_bonds = pd.read_html(str(table_corporate_bonds))[0]
-    # Save the data to a CSV file
-    filename_corporate_bonds = 'ishares-global-corporate-bond-$.csv'
-    df_corporate_bonds.to_csv(filename_corporate_bonds, index=False)
-    #PUBLIC BONDS
-    response_public_bonds = requests.post(url_public_bonds, data=params)
-    soup_public_bonds = BeautifulSoup(response_public_bonds.content, 'html.parser')
-    table_public_bonds = soup_public_bonds.find_all('table')[0]
-    df_public_bonds = pd.read_html(str(table_public_bonds))[0]
-    # Save the data to a CSV file
-    filename_public_bonds = 'db-x-trackers-ii-global-sovereign-5.csv'
-    df_public_bonds.to_csv(filename_public_bonds, index=False)
-    #GOLD
-    response_gold = requests.post(url_gold, data=params)
-    soup_gold = BeautifulSoup(response_gold.content, 'html.parser')
-    table_gold = soup_gold.find_all('table')[0]
-    df_gold = pd.read_html(str(table_gold))[0]
-    # Save the data to a CSV file
-    filename_gold = 'spdr-gold-trust.csv'
-    df_gold.to_csv(filename_gold, index=False)
-    #CASH
-    response_cash = requests.post(url_cash, data=params)
-    soup_cash = BeautifulSoup(response_cash.content, 'html.parser')
-    table_cash = soup_cash.find_all('table')[0]
-    df_cash = pd.read_html(str(table_cash))[0]
-    # Save the data to a CSV file
-    filename_cash = 'usdollar.csv'
-    df_cash.to_csv(filename_cash, index=False)
-
+    #assets = ['funds/amundi-msci-wrld-ae-c', 'etfs/ishares-global-corporate-bond-$',
+    #          'etfs/db-x-trackers-ii-global-sovereign-5', 'etfs/spdr-gold-trust', 'indices/usdollar']
 
