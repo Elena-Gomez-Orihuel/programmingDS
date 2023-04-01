e4df55d7a2efb80ddaf37e0357c99585b90e1ba6
Elena-Gomez-Orihuel <elenamgomezorihuel@gmail.com>
2023-04-01 13:19:27 +0200

    part 1 finished (i hope)

--- main.py
+++ main.py
@@ -1,246 +1,64 @@
-import csv
-import time
-import pandas as pd
-import re
-from calendar import monthrange
-import datetime
+import tkinter as tk
+import os
+from PIL import Image, ImageTk
+#path variables
+# Get the current working directory
+cwd = os.getcwd()
+# Construct the path to the images directory
+images_dir = os.path.join(cwd, 'images')
+# Access an image file within the images directory
+grey_tick = os.path.join(images_dir, 'grey_tick.png')
+green_tick = os.path.join(images_dir, 'green_tick.png')
+# Function to execute part1.py
+def execute_part1():
+    os.system('python part1.py')
+    tick_img = Image.open(green_tick).resize((50,50))
+    tick_icon = ImageTk.PhotoImage(tick_img)
+    tick_label1.config(image=tick_icon)
+    tick_label1.image = tick_icon
 
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.firefox.service import Service
-from selenium.webdriver.common.keys import Keys
-import warnings
-from selenium.common.exceptions import TimeoutException, ElementNotInteractableException
-def click_not_login(driver):
-    try:
-        button = WebDriverWait(driver, 5).until(
-            EC.presence_of_element_located((By.CLASS_NAME, 'popupCloseIcon.largeBannerCloser'))
-        )
-        if button is not None:
-            button.click()
-        else:
-            pass
-        print('Button clicked')
-    except TimeoutException:
-        print('Button not found, continuing...')
-    except ElementNotInteractableException:
-        # Handle the exception
-        print('Element is not interactable')
+# Function to execute part2.py
+def execute_part2():
+    os.system('python part2.py')
+    tick_img = Image.open(green_tick).resize((50,50))
+    tick_icon = ImageTk.PhotoImage(tick_img)
+    tick_label2.config(image=tick_icon)
+    tick_label2.image = tick_icon
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print("Starting")
-    warnings.filterwarnings("ignore", category=DeprecationWarning)
-    #Navigation options
-    options = webdriver.FirefoxOptions()
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
-    options.add_argument("--start-maximized")
+# Function to execute part3.py
+def execute_part3():
+    os.system('python part3.py')
+    tick_img = Image.open(green_tick).resize((50,50))
+    tick_icon = ImageTk.PhotoImage(tick_img)
+    tick_label3.config(image=tick_icon)
+    tick_label3.image = tick_icon
 
-    #Opening investing.com
-    from webdriver_manager.firefox import GeckoDriverManager
+# Create the main window
+root = tk.Tk()
+root.geometry('200x250')
 
-    assets = [['etfs/ishares-global-corporate-bond-$','ishares-global-corporate-bond-$.csv'],
-              ['funds/amundi-msci-wrld-ae-c','amundi-msci-wrld-ae-c.csv'],
-              ['etfs/db-x-trackers-ii-global-sovereign-5','db-x-trackers-ii-global-sovereign-5.csv'],
-              ['etfs/spdr-gold-trust','spdr-gold-trust.csv'],
-              ['indices/usdollar','usdollar.csv']]
-    """
-    https://www.investing.com/etfs/etfs/ishares-global-corporate-bond-$
-    https://www.investing.com/etfs/db-x-trackers-ii-global-sovereign-5
-    https://www.investing.com/etfs/spdr-gold-trust
-    https://www.investing.com/indices/usdollar
-    
-    https://www.investing.com/funds/amundi-msci-wrld-ae-c
-    """
-    with webdriver.Firefox(service=Service(executable_path=GeckoDriverManager().install()), options=options) as driver:
-        firstTime = True
-        table_data = []
-        table = ""
-        for asset in assets:
-            print("WEB")
-            print(asset[0])
-            print("CSV")
-            print(asset[1])
+# Create the buttons and icons
+button1 = tk.Button(root, text='PART 1', command=execute_part1)
+button2 = tk.Button(root, text='PART 2', command=execute_part2)
+button3 = tk.Button(root, text='PART 3', command=execute_part3)
 
-            driver.get("https://www.investing.com/" + asset[0])
+tick_img = Image.open(grey_tick).resize((50,50))
+tick_icon = ImageTk.PhotoImage(tick_img)
+tick_label1 = tk.Label(root, image=tick_icon)
+tick_label2 = tk.Label(root, image=tick_icon)
+tick_label3 = tk.Label(root, image=tick_icon)
 
-            if(firstTime):
-                #Cookies button
-                cookies_button = WebDriverWait(driver,60).until(EC.element_to_be_clickable((By.ID,"onetrust-accept-btn-handler")))
-                cookies_button.click()
-                firstTime = False
-            """
-            #----------------------------------------------------------------------------------------------------------------------
-            #login
-            time.sleep(2)
-            #inv-button user-area_link__Xa7Br
-            login_button = WebDriverWait(driver, 60).until(
-                EC.element_to_be_clickable((By.CLASS_NAME, "inv-button.user-area_link__Xa7Br")))
-            login_button.click()
-            #login with email
-            #inv-button social-auth-button_button__FiobW social-auth-button_icon__PRIVZ social-auth-button_email__o7pnA signin_button__5lDBE
-            email_button = WebDriverWait(driver, 60).until(
-                EC.element_to_be_clickable((By.CLASS_NAME, "inv-button.social-auth-button_button__FiobW.social-auth-button_icon__PRIVZ.social-auth-button_email__o7pnA.signin_button__5lDBE")))
-            email_button.click()
-            #enter email
-            #input_input__FGe3c
-            email_input = WebDriverWait(driver, 60).until(
-                EC.element_to_be_clickable((By.CLASS_NAME, "input_input__FGe3c")))
-            email_input.send_keys("eleniliagomez@outlook.es")
+# Add the buttons and icons to the window using grid layout
+button1.grid(row=0, column=0, padx=10, pady=10)
+tick_label1.grid(row=0, column=1, padx=10, pady=10)
+button2.grid(row=1, column=0, padx=10, pady=10)
+tick_label2.grid(row=1, column=1, padx=10, pady=10)
+button3.grid(row=2, column=0, padx=10, pady=10)
+tick_label3.grid(row=2, column=1, padx=10, pady=10)
 
-            #enter pwd
-            pwd_input = WebDriverWait(driver, 60).until(
-                EC.element_to_be_clickable((By.CLASS_NAME, "input_input__FGe3c.input_password__B_iju")))
-            pwd_input.send_keys("1d8715d6elena")
+# Center the column of widgets
+root.grid_columnconfigure(0, weight=1)
+root.grid_columnconfigure(1, weight=1)
 
-            #click loginbutton
-            #inv-button signin_primaryBtn__Z16kI mb-4
-            final_login = WebDriverWait(driver, 60).until(
-                EC.element_to_be_clickable((By.CLASS_NAME,
-                                            "inv-button.signin_primaryBtn__Z16kI.mb-4")))
-            final_login.click()
-            #---------------------------------------------------------------------------------------------------------------------------------------------
-            """
-            #Historical data button
-            historical_data_button = WebDriverWait(driver,60).until(EC.element_to_be_clickable((By.LINK_TEXT,"Historical Data")))
-            historical_data_button.click()
-            click_not_login(driver)
-            if asset[0] == 'funds/amundi-msci-wrld-ae-c':
-                print("holIF")
-                # Set the dates
-                new_start_date = "2020-01-01"
-                new_end_date = "2020-12-31"
-                # Date icon button
-                date_button = WebDriverWait(driver, 20).until(
-                    EC.element_to_be_clickable((By.ID, "widget")))
-                date_button.click()
-
-                click_not_login(driver)
-
-                start_date_input = driver.find_element(By.ID, 'startDate')
-                start_date_input.clear()
-                start_date_input.send_keys(new_start_date)
-
-                end_date_input = driver.find_element(By.ID, 'endDate')
-                end_date_input.clear()
-                end_date_input.send_keys(new_end_date)
-
-                # Apply button
-                apply_button = WebDriverWait(driver, 60).until(
-                    EC.element_to_be_clickable((By.ID, "applyBtn")))
-                apply_button.click()
-                time.sleep(10)
-
-                click_not_login(driver)
-
-                # Create a list of lists containing the data from the table
-                table = driver.find_element(By.ID, 'curr_table')
-                if table is not None:
-                    print("yay we have a table")
-                else:
-                    print("Table element not found")
-
-                rows = table.find_elements(By.TAG_NAME, 'tr')
-                print("GOT THE TABLE")
-                # loop through each row
-                for row in rows:
-                    row_data = []
-                    # find all the cells in the row
-                    cells = row.find_elements(By.TAG_NAME, 'td')
-                    # loop through each cell
-                    for cell in cells:
-                        row_data.append(cell.text)
-                    # append the row data to the table data
-                    table_data.append(row_data)
-
-                # creating the dataframe and creating the csv
-                df = pd.DataFrame(table_data)
-                print("1")
-                print(df.head(5))
-                df = df.drop(0)
-                print("2")
-                print(df.head(5))
-                df = df.drop([2, 3, 4], axis=1)
-                print("3")
-                print(df.head(5))
-                # assign column names
-                df.columns = ['Date', 'Price', 'Change']
-                print("4")
-                print(df.head(5))
-                # types of the columns
-                result = df.dtypes
-                print(result)
-                df.to_csv(asset[1], index=False)
-                table_data = []
-
-            else:
-                print("else")
-                # Set the dates
-                new_start_date = "2020-01-02"
-                new_end_date = "2021-01-01"
-                #Date icon button
-                date_button = WebDriverWait(driver,20).until(EC.element_to_be_clickable((By.CLASS_NAME,"DatePickerWrapper_input__MDvWH")))
-                date_button.click()
-
-                start_date_input = driver.find_element(By.XPATH, '//*[@id="__next"]/div[2]/div/div/div[2]/main/div/div[4]/div/div/div[2]/div[2]/div[2]/div[2]/div[1]/div/div[1]/input')
-                start_date_input.clear()
-                start_date_input.send_keys(new_start_date)
-
-                end_date_input = driver.find_element(By.CSS_SELECTOR, 'div.NativeDateInput_root__wbgyP:nth-child(2) > input:nth-child(1)')
-                end_date_input.clear()
-                end_date_input.send_keys(new_end_date)
-
-                # Apply button
-                apply_button = WebDriverWait(driver, 60).until(
-                    EC.element_to_be_clickable((By.CLASS_NAME, "inv-button.HistoryDatePicker_apply-button__fPr_G")))
-                apply_button.click()
-                time.sleep(10)
-
-                click_not_login(driver)
-
-                # Create a list of lists containing the data from the table
-                table = driver.find_element("xpath",'//*[@data-test="historical-data-table"]')
-
-                if table is not None:
-                    print("yay we have a table")
-                else:
-                    print("Table element not found")
-
-                rows = table.find_elements(By.TAG_NAME, 'tr')
-                print("GOT THE TABLE")
-                # loop through each row
-                for row in rows:
-                    row_data = []
-                    # find all the cells in the row
-                    cells = row.find_elements(By.TAG_NAME, 'td')
-                    # loop through each cell
-                    for cell in cells:
-                        row_data.append(cell.text)
-                    # append the row data to the table data
-                    table_data.append(row_data)
-
-                #creating the dataframe and creating the csv
-                df = pd.DataFrame(table_data)
-                print("1")
-                print(df.head(5))
-                df = df.drop(0)
-                print("2")
-                print(df.head(5))
-                df = df.drop([2, 3, 4], axis=1)
-                print("3")
-                print(df.head(5))
-                # assign column names
-                df.columns = ['Date', 'Price', 'Vol', 'Change']
-                print("4")
-                print(df.head(5))
-                #types of the columns
-                result = df.dtypes
-                print(result)
-                df.to_csv(asset[1], index=False)
-                table_data = []
-        time.sleep(100)
+# Start the GUI event loop
+root.mainloop()
