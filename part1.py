0d72147b3e67806f915aa76070fd2fc6f388521c
Jaime-gc-00 <78904735+Jaime-gc-00@users.noreply.github.com>
2023-04-06 14:42:50 +0200

    Add files via upload

---
+++ part1.py
@@ -0,0 +1,236 @@
+import csv
+import time
+import pandas as pd
+import re
+from calendar import monthrange
+import datetime
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.common.keys import Keys
+import warnings
+from selenium.common.exceptions import TimeoutException, ElementNotInteractableException
+
+
+def click_not_login(driver):
+    try:
+        button = WebDriverWait(driver, 5).until(
+            EC.presence_of_element_located((By.CLASS_NAME, 'popupCloseIcon.largeBannerCloser'))
+        )
+        if button is not None:
+            button.click()
+        else:
+            pass
+        print('Button clicked')
+    except TimeoutException:
+        print('Button not found, continuing...')
+    except ElementNotInteractableException:
+        # Handle the exception
+        print('Element is not interactable')
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print("Starting")
+    warnings.filterwarnings("ignore", category=DeprecationWarning)
+    # Navigation options
+    options = webdriver.FirefoxOptions()
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument("--start-maximized")
+
+    # Opening investing.com
+    from webdriver_manager.firefox import GeckoDriverManager
+
+    assets = [['etfs/ishares-global-corporate-bond-$', 'ishares-global-corporate-bond-$.csv'],
+              ['funds/amundi-msci-wrld-ae-c', 'amundi-msci-wrld-ae-c.csv'],
+              ['etfs/db-x-trackers-ii-global-sovereign-5', 'db-x-trackers-ii-global-sovereign-5.csv'],
+              ['etfs/spdr-gold-trust', 'spdr-gold-trust.csv'],
+              ['indices/usdollar', 'usdollar.csv']]
+
+    """   
+    https://www.investing.com/etfs/etfs/ishares-global-corporate-bond-$
+    https://www.investing.com/etfs/db-x-trackers-ii-global-sovereign-5
+    https://www.investing.com/etfs/spdr-gold-trust
+    https://www.investing.com/indices/usdollar
+    https://www.investing.com/funds/amundi-msci-wrld-ae-c
+    """
+    with webdriver.Firefox(service=Service(executable_path=GeckoDriverManager().install()), options=options) as driver:
+        firstTime = True
+        table_data = []
+        table = ""
+        for asset in assets:
+            print("WEB")
+            print(asset[0])
+            print("CSV")
+            print(asset[1])
+
+            driver.get("https://www.investing.com/" + asset[0])
+
+            if (firstTime):
+                # Cookies button
+                cookies_button = WebDriverWait(driver, 60).until(
+                    EC.element_to_be_clickable((By.ID, "onetrust-accept-btn-handler")))
+                cookies_button.click()
+                firstTime = False
+            """
+            #----------------------------------------------------------------------------------------------------------------------
+            #login
+            time.sleep(2)
+            #inv-button user-area_link__Xa7Br
+            login_button = WebDriverWait(driver, 60).until(
+                EC.element_to_be_clickable((By.CLASS_NAME, "inv-button.user-area_link__Xa7Br")))
+            login_button.click()
+            #login with email
+            #inv-button social-auth-button_button__FiobW social-auth-button_icon__PRIVZ social-auth-button_email__o7pnA signin_button__5lDBE
+            email_button = WebDriverWait(driver, 60).until(
+                EC.element_to_be_clickable((By.CLASS_NAME, "inv-button.social-auth-button_button__FiobW.social-auth-button_icon__PRIVZ.social-auth-button_email__o7pnA.signin_button__5lDBE")))
+            email_button.click()
+            #enter email
+            #input_input__FGe3c
+            email_input = WebDriverWait(driver, 60).until(
+                EC.element_to_be_clickable((By.CLASS_NAME, "input_input__FGe3c")))
+            email_input.send_keys("eleniliagomez@outlook.es")
+            #enter pwd
+            pwd_input = WebDriverWait(driver, 60).until(
+                EC.element_to_be_clickable((By.CLASS_NAME, "input_input__FGe3c.input_password__B_iju")))
+            pwd_input.send_keys("1d8715d6elena")
+            #click loginbutton
+            #inv-button signin_primaryBtn__Z16kI mb-4
+            final_login = WebDriverWait(driver, 60).until(
+                EC.element_to_be_clickable((By.CLASS_NAME,
+                                            "inv-button.signin_primaryBtn__Z16kI.mb-4")))
+            final_login.click()
+            #---------------------------------------------------------------------------------------------------------------------------------------------
+            """
+            # Historical data button
+            historical_data_button = WebDriverWait(driver, 60).until(
+                EC.element_to_be_clickable((By.LINK_TEXT, "Historical Data")))
+            historical_data_button.click()
+            click_not_login(driver)
+            if asset[0] == 'funds/amundi-msci-wrld-ae-c':
+                #print("holIF")
+                # Set the dates
+                new_start_date = "01/01/2020"
+                new_end_date = "12/31/2020"
+                # Date icon button
+                date_button = WebDriverWait(driver, 20).until(
+                    EC.element_to_be_clickable((By.ID, "widget")))
+                date_button.click()
+
+                click_not_login(driver)
+
+                start_date_input = driver.find_element(By.ID, 'startDate')
+                start_date_input.clear()
+                start_date_input.send_keys(new_start_date)
+
+                end_date_input = driver.find_element(By.ID, 'endDate')
+                end_date_input.clear()
+                end_date_input.send_keys(new_end_date)
+
+                # Apply button
+                apply_button = WebDriverWait(driver, 60).until(
+                    EC.element_to_be_clickable((By.ID, "applyBtn")))
+                apply_button.click()
+                time.sleep(10)
+
+                click_not_login(driver)
+
+                # Create a list of lists containing the data from the table
+                table = driver.find_element(By.ID, 'curr_table')
+                if table is not None:
+                    print("yay we have a table")
+                else:
+                    print("Table element not found")
+
+                rows = table.find_elements(By.TAG_NAME, 'tr')
+                # loop through each row
+                for row in rows:
+                    row_data = []
+                    # find all the cells in the row
+                    cells = row.find_elements(By.TAG_NAME, 'td')
+                    # loop through each cell
+                    for cell in cells:
+                        row_data.append(cell.text)
+                    # append the row data to the table data
+                    table_data.append(row_data)
+
+                # creating the dataframe and creating the csv
+                df = pd.DataFrame(table_data)
+                df = df.drop(0)
+                df = df.drop([2, 3, 4], axis=1)
+                # assign column names
+                df.columns = ['Date', 'Price', 'Change']
+                # types of the columns
+                result = df.dtypes
+                print(result)
+                df.to_csv(asset[1], index=False)
+                table_data = []
+                print('Finish: ', asset[1])
+
+            else:
+                # Set the dates
+                new_start_date = "2020-01-01"
+                new_end_date = "2021-01-01"
+                # Date icon button
+                date_button = WebDriverWait(driver, 20).until(
+                    EC.element_to_be_clickable((By.CLASS_NAME, "DatePickerWrapper_input__MDvWH")))
+                date_button.click()
+
+                start_date_input = driver.find_element(By.XPATH,
+                                                       '//*[@id="__next"]/div[2]/div/div/div[2]/main/div/div[4]/div/div/div[2]/div[2]/div[2]/div[2]/div[1]/div/div[1]/input')
+                start_date_input.clear()
+                start_date_input.send_keys(new_start_date)
+
+                end_date_input = driver.find_element(By.CSS_SELECTOR,
+                                                     'div.NativeDateInput_root__wbgyP:nth-child(2) > input:nth-child(1)')
+                end_date_input.clear()
+                end_date_input.send_keys(new_end_date)
+
+                # Apply button
+                apply_button = WebDriverWait(driver, 60).until(
+                    EC.element_to_be_clickable((By.CLASS_NAME, "inv-button.HistoryDatePicker_apply-button__fPr_G")))
+                apply_button.click()
+                time.sleep(10)
+
+                click_not_login(driver)
+
+                # Create a list of lists containing the data from the table
+                table = driver.find_element("xpath", '//*[@data-test="historical-data-table"]')
+
+                if table is not None:
+                    print("yay we have a table")
+                else:
+                    print("Table element not found")
+
+                rows = table.find_elements(By.TAG_NAME, 'tr')
+                print("GOT THE TABLE")
+                # loop through each row
+                for row in rows:
+                    row_data = []
+                    # find all the cells in the row
+                    cells = row.find_elements(By.TAG_NAME, 'td')
+                    # loop through each cell
+                    for cell in cells:
+                        row_data.append(cell.text)
+                    # append the row data to the table data
+                    table_data.append(row_data)
+
+                # creating the dataframe and creating the csv
+                df = pd.DataFrame(table_data)
+                df = df.drop(0)
+                df = df.drop([2, 3, 4], axis=1)
+                # assign column names
+                df.columns = ['Date', 'Price', 'Vol', 'Change']
+                # types of the columns
+                result = df.dtypes
+                print(result)
+                df.to_csv(asset[1], index=False)
+                table_data = []
+                print('Finish: ', asset[1])
+        time.sleep(20)
---
+++ part2.py
@@ -0,0 +1,172 @@
+import csv
+from itertools import product
+from math import factorial
+import pandas as pd
+import statistics
+from math import sqrt
+from datetime import datetime
+
+######################### Portforlio allocations ################################
+def num_combinations(m, n):
+    return factorial(m+n-1) // (factorial(n) * factorial(m - 1))
+
+def create_portfolio_allocations_df(assets, delta):
+    total = 100
+    num_pieces = total // delta
+    if num_pieces == 0 or total % delta != 0:
+        raise ValueError("Invalid delta value. Please, select a valid delta value between: 1, 2, 4, 5, 10, 20, 25, 50 and 100")
+    combinations = list(product(range(num_pieces+1), repeat=len(assets)))
+    rows = []
+    for combination in combinations:
+        if sum(combination) == num_pieces:
+            row = [int(x*delta) for x in combination]
+            rows.append(row)
+    df = pd.DataFrame(rows, columns=assets)
+    print('Number of combinations in the portfolio: ' + str(num_combinations(len(assets), num_pieces)))
+    return df
+
+
+################## Portforlio performance #######################
+
+################## Support functions #######################
+# This function helps read a CSV file independently of its format, and ensures that the file has more than one column. 
+# It returns the resulting pandas DataFrame.
+def read_csv_file(filename):
+    try:
+        df = pd.read_csv(filename)
+        if df.shape[1] <= 1:
+            raise ValueError("The file has 1 or fewer columns")
+    except:
+        try:
+            df = pd.read_csv(filename, sep='\t', encoding='utf-8', decimal='.')
+            if df.shape[1] <= 1:
+                raise ValueError("The file has 1 or fewer columns")
+        except:
+            print(f"Error while reading the file {filename}")
+            return None
+    return df
+
+# This function helps parse a date string independently of its format, and returns a datetime object.
+def parse_date(date_str):
+    try:
+        return datetime.strptime(date_str, '%m/%d/%Y')
+    except ValueError:
+        return datetime.strptime(date_str, '%b %d, %Y')
+    
+
+############################## Return ######################################
+def calculate_return(portfolio_allocations):
+
+    # Read in the share prices from separate CSV files for each asset
+    ST_prices = read_csv_file("amundi-msci-wrld-ae-c.csv")
+    PB_prices = read_csv_file("db-x-trackers-ii-global-sovereign-5.csv")
+    CB_prices = read_csv_file("ishares-global-corporate-bond-$.csv")
+    GO_prices = read_csv_file("spdr-gold-trust.csv")
+    CA_prices = read_csv_file("usdollar.csv")
+
+    # Extract the share prices as of 01/01/2020
+    ST_price_buy = ST_prices["Price"].values[-1]
+    PB_price_buy = PB_prices["Price"].values[-1]
+    CB_price_buy = CB_prices["Price"].values[-1]
+    GO_price_buy = GO_prices["Price"].values[-1]
+    CA_price_buy = CA_prices["Price"].values[-1]
+
+    # Extract the share prices as of 12/31/2020
+    ST_prices_current = ST_prices["Price"].values[0]
+    PB_prices_current = PB_prices["Price"].values[0]
+    CB_price_current = CB_prices["Price"].values[0]
+    GO_price_current = GO_prices["Price"].values[0]
+    CA_price_current = CA_prices["Price"].values[0]
+
+    # Create an empty DataFrame to store the portfolio returns
+    return_list=[]
+    # Loop over each row in the portfolio allocations DataFrame
+    for i in range(len(portfolio_allocations)):
+        # Extract the portfolio allocations for the current row
+        portfolio = portfolio_allocations.iloc[i].to_dict()
+        
+        # Calculate the total buy amount and current value of the portfolio
+        buy_amount = (portfolio["ST"] * ST_price_buy) + (portfolio["CB"] * CB_price_buy) + (portfolio["PB"] * PB_price_buy) + (portfolio["GO"] * GO_price_buy) + (portfolio["CA"] * CA_price_buy)
+        current_value = (portfolio["ST"] * ST_prices_current) + (portfolio["CB"] * CB_price_current) + (portfolio["PB"] * PB_prices_current) + (portfolio["GO"] * GO_price_current) + (portfolio["CA"] * CA_price_current)
+        
+        # Calculate the portfolio return as a percentage
+        portfolio_return = ((current_value - buy_amount) / buy_amount) * 100
+        
+        # Add the portfolio return to the list
+        return_list.append(portfolio_return)
+
+    # Return the list of returns values
+    return return_list    
+
+############################ Volatility ################################
+def calculate_volatility(portfolio_allocations):
+
+    # Read in the share prices from separate CSV files for each asset
+    ST_df = read_csv_file("amundi-msci-wrld-ae-c.csv")
+    PB_df = read_csv_file("db-x-trackers-ii-global-sovereign-5.csv")
+    CB_df = read_csv_file("ishares-global-corporate-bond-$.csv")
+    GO_df = read_csv_file("spdr-gold-trust.csv")
+    CA_df = read_csv_file("usdollar.csv")
+    
+    ST_df['Parsed_Date'] = ST_df['Date'].apply(parse_date)
+    PB_df['Parsed_Date'] = PB_df['Date'].apply(parse_date)
+    CB_df['Parsed_Date'] = CB_df['Date'].apply(parse_date)
+    GO_df['Parsed_Date'] = GO_df['Date'].apply(parse_date)
+    CA_df['Parsed_Date'] = CA_df['Date'].apply(parse_date)
+
+
+    all_dates = ST_df.merge(PB_df, on='Parsed_Date', how='inner', suffixes=('_ST', '_PB')).merge(CB_df, on='Parsed_Date', how='inner', suffixes=('_PB', '_CB')).merge(GO_df, on='Parsed_Date', how='inner', suffixes=('_CB', '_GO')).merge(CA_df, on='Parsed_Date', how='inner', suffixes=('_GO', '_CA'))
+    # Crear una máscara booleana para cada dataset indicando si la fecha está presente en la lista de fechas únicas
+    mask1 = ST_df['Parsed_Date'].isin(all_dates['Parsed_Date'])
+    mask2 = PB_df['Parsed_Date'].isin(all_dates['Parsed_Date'])
+    mask3 = CB_df['Parsed_Date'].isin(all_dates['Parsed_Date'])
+    mask4 = GO_df['Parsed_Date'].isin(all_dates['Parsed_Date'])
+    mask5 = CA_df['Parsed_Date'].isin(all_dates['Parsed_Date'])
+
+    ST_df = ST_df[mask1]
+    PB_df = PB_df[mask2]
+    CB_df = CB_df[mask3]
+    GO_df = GO_df[mask4]
+    CA_df = CA_df[mask5]
+
+    ST_prices = ST_df["Price"].values
+    PB_prices = PB_df["Price"].values
+    CB_prices = CB_df["Price"].values
+    GO_prices = GO_df["Price"].values
+    CA_prices = CA_df["Price"].values
+
+    # Create an empty DataFrame to store the values of Value
+    VOLAT=[]
+    # Loop over each row in the portfolio allocations DataFrame
+    for i in range(len(portfolio_allocations)):
+        # Extract the portfolio allocations for the current row
+        values_porfolio = []
+        portfolio = portfolio_allocations.iloc[i].to_dict()
+        for j in range(len(ST_prices)):
+            value = (portfolio["ST"] * ST_prices[j]) + (portfolio["CB"] *CB_prices[j] ) + (portfolio["PB"] * PB_prices[j]) + (portfolio["GO"] * GO_prices[j]) + (portfolio["CA"] * CA_prices[j])
+            values_porfolio.append(value)
+        #media
+        portfolio_mean = statistics.mean(values_porfolio)
+        #desviacion
+        #portfolio_stdev = statistics.stdev(values_porfolio)
+        portfolio_stdev = sqrt(sum((x - portfolio_mean)**2 for x in values_porfolio) / (len(values_porfolio)))
+
+        #volatility
+        volatility= (portfolio_stdev/portfolio_mean)*100
+        VOLAT.append(volatility)
+    return VOLAT
+
+############################## Metrics ######################################
+def calculate_metrics(portfolio_allocations_df):
+    portfolio_allocations_df['RETURN'] = calculate_return(portfolio_allocations_df)
+    portfolio_allocations_df['VOLAT'] = calculate_volatility(portfolio_allocations_df)
+    portfolio_allocations_df.to_csv("portfolio_metrics.csv", index=False) 
+
+############### Main ejecution ####################
+if __name__ == '__main__':
+    # Crating the portfolio
+    assets = ['ST', 'CB', 'PB', 'GO', 'CA']
+    delta = 20
+    portfolio_allocations_df = create_portfolio_allocations_df(assets, delta)
+    # Computing Return and Volatility
+    calculate_metrics(portfolio_allocations_df)
